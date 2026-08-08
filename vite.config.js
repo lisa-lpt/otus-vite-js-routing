8c7495aea998765680cba647a60cbd0f3efe06a1
lisa-lpt <lptelizaveta@gmail.com>
2026-08-08 23:03:08 +0300

    fix vite base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ const repoName = process.env.REPO_NAME || '';
 const BASE_PATH = repoName ? `/${repoName}/` : '/';
 
 export default defineConfig({
+  base: BASE_PATH,
   define: {
     __BASE_PATH__: JSON.stringify(BASE_PATH),
   },
